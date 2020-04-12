304526c12f2f1e4d85c3dbe1de2f82bebb86da74
Satyajit Roy <kodelint@gmail.com>
2020-04-12 12:41:18 -0700

    new: Added yum-ius repo for CentOS 7 and Python3.6

--- Berksfile
+++ Berksfile
@@ -3,3 +3,4 @@ source "https://supermarket.chef.io"
 metadata
 
 cookbook 'apt', '~> 7.0'
+cookbook 'yum-ius', '~> 3.1.0'
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Satyajit Roy'
 maintainer_email 'kodelint@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures airflow-cookbook'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 14.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
@@ -22,3 +22,4 @@ supports         'ubuntu', '= 18.04'
 supports         'centos', '= 7.0'
 
 depends 'apt'
+depends 'yum-ius'
